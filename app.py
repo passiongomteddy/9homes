5cf4a1fee47cdc10d092703981dbe017ff5aaa99
passiongomteddy <51498908+passiongomteddy@users.noreply.github.com>
2019-06-15 12:07:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-		return 'hello world!'
+		return 'hello world! Dongjin'
 
 if __name__ == '__main__':
 		app.run('0.0.0.0', port=5000, debug=True)
